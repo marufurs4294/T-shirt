5e6b6ca8a2090f0c19920429a8dbb1c83901eb8b
marufurs4294 <siyamm4294@gmail.com>
2023-01-10 03:20:41 +0600

    added product  name

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,6 +5,14 @@ const Cart = ({cart}) => {
         <div className='mt-4'>
             <h1>Order Summary</h1>
             <h4 className='mt-4'>Order Quantity: {cart.length}</h4>
+            <div className='mt-4'>
+            {
+                cart.map(tshirt => <h5 className='mt-3' key={tshirt._id}>
+                    {tshirt.name}
+                </h5>)
+            }
+            </div>
+            
         </div>
     );
 };
