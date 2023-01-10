aad75b498898ef324457a063e113f496d645256f
marufurs4294 <siyamm4294@gmail.com>
2023-01-10 18:27:00 +0600

    buying quote

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 const Cart = ({cart}) => {
+    let massage;
+    if(cart.length === 0){
+        massage = <h5>Please Buy at least one item</h5>
+    }
     return (
         <div className='mt-4 position-fixed'>
             <h1>Order Summary</h1>
@@ -10,6 +14,9 @@ const Cart = ({cart}) => {
                 cart.map(tshirt => <h5 className='mt-3' key={tshirt._id}>
                     {tshirt.name}
                 </h5>)
+            }
+            {
+                massage
             }
             </div>
             
